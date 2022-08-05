0432cd041332adec9b41475f1ac698bb5d35d70e
DEF4LT-303 <ryanrafi72@gmail.com>
2022-08-05 11:56:34 +0600

    requestTime middleware added

--- server/index.js
+++ server/index.js
@@ -11,6 +11,11 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
