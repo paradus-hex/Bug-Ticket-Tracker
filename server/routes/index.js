c676b6d48edc740e76276b7a77f9b82cf94afecb
DEF4LT-303 <ryanrafi72@gmail.com>
2022-08-07 23:19:02 +0600

    refactored imports

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,24 @@
-const projectRoutes = require("./project");
-const userRoutes = require("./user");
-const userProjectRoutes = require("./userProjects");
-const ticketRoutes = require("./ticket");
-const devAssignmentsRoutes = require("./devAssignments");
-const loginRoutes = require("./login");
-const availableUsersRoutes = require("./availableUsers");
-const authRoutes = require("./auth");
-const RootRouter = require("express").Router();
+// const projectRoutes = require("./project");
+// const userRoutes = require("./user");
+// const userProjectRoutes = require("./userProjects");
+// const ticketRoutes = require("./ticket");
+// const devAssignmentsRoutes = require("./devAssignments");
+// const loginRoutes = require("./login");
+// const availableUsersRoutes = require("./availableUsers");
+// const authRoutes = require("./auth");
+// const RootRouter = require("express").Router();
+
+import RootRouter from "express";
+import authRoutes from "./auth";
+import availableUsersRoutes from "./availableUsers";
+import devAssignmentsRoutes from "./devAssignments";
+import loginRoutes from "./login";
+import projectRoutes from "./project";
+import ticketRoutes from "./ticket";
+import userRoutes from "./user";
+import userProjectRoutes from "./userProjects";
+
+const router = RootRouter();
 
 // 3) ROUTES
 // router.use('/api/v1/tickets', ticketRouter);
