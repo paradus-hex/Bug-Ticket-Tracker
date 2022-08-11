c0ec559e2fa929d527630f1a05d8072df3cb24e0
Ahsanul Islam <64695124+ahsanul20101525@users.noreply.github.com>
2022-08-11 20:09:35 +0600

    Made codes cleaner

--- server/routes/ticket.js
+++ server/routes/ticket.js
@@ -3,37 +3,20 @@ import ticketController from './../controllers/ticketController.js';
 
 const router = Router()
 
-// router.param('id', ticketController.checkID);
 
 router
     .route('/')
     .get(ticketController.getAll);
-// .post(ticketController.checkBody, ticketController.createTicket);
 
 router
     .route("/:projectId/:ticketId")
     .get(ticketController.getTicket);
 
-//   .patch(ticketController.updateTicket)
-//   .delete(ticketController.deleteTicket);
 
-// module.exports = router;
 router
     .route('/:projectId')
     .get(ticketController.getProjectTickets);
 
-// Matches route with "/api/v1/tickets/"
-// router.route("/").get(authorization, ticketController.getUserTickets);
 
-// router
-//   .route("/:projectId")
-//   .post(authorization, ticketController.createTicket)
-//   .get(ticketController.getProjectTickets);
-
-// router
-//   .route("/:projectId/:ticketId")
-//   .get(ticketController.getTicket)
-//   .put(authorization, ticketController.updateTicket)
-//   .delete(authorization, ticketController.deleteTicket);
 
 export default router;
