a47380a58d4a15f23235895c58a499eaacd80193
Yaseen Nur <paradushex@gmail.com>
2022-08-21 16:12:18 +0600

    modified userModel

--- client/components/pages/users/Users.jsx
+++ client/components/pages/users/Users.jsx
@@ -20,16 +20,15 @@ function Users() {
   const { users } = data?.data;
 
   const columns = [
-    { headerName: 'ID', field: 'user_id', width: 150 },
-    { headerName: 'Name', field: 'name', width: 150 },
-    { headerName: 'Email', field: 'email', width: 150 },
-    { headerName: 'Role', field: 'user_authority', width: 150 }
+    { headerName: 'ID', field: 'user_id', flex: 1 },
+    { headerName: 'Name', field: 'name', flex: 1 },
+    { headerName: 'Email', field: 'email', flex: 1 },
+    { headerName: 'Role', field: 'user_authority', flex: 1 }
   ];
 
   return (
     <Box sx={{ height: 500, width: '100%' }}>
-      <h1 align='center'>React-App</h1>
-      <h4 align='center'>Material Table with CRUD operation</h4>
+      <h1 align='center'>Users Information</h1>
       <DataGrid
         rows={users}
         columns={columns}
